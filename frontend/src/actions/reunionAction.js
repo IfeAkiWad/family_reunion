377f0853e32853961b63e7573148e32936cd08ae
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-11-10 08:30:26 -0600

    reunion action: axios

--- frontend/src/actions/reunionAction.js
+++ frontend/src/actions/reunionAction.js
@@ -1,10 +1,24 @@
-export const fetchReunions = () => {
-    return (dispatch) => {
-      dispatch({ type: "LOADING_REUNIONS" });
-      fetch("http://localhost:3000/reunions")
-        .then(response => response.json())
-        .then((data) => {
-          dispatch({ type: "ADD_REUNIONS", reunions: data });
-        });
-    };
-  };
+import axios from 'axios'
+
+// export const fetchReunions = () => {
+//     return (dispatch) => {
+//       dispatch({ type: "LOADING_REUNIONS" });
+//       fetch("http://localhost:3000/reunions")
+//         .then(response => response.json())
+//         .then((data) => {
+//           dispatch({ type: "ADD_REUNIONS", reunions: data });
+//         });
+//     };
+//   };
+
+  
+
+    export const fetchReunions = () => {
+        return (dispatch) => {
+            dispatch({ type: "LOADING_REUNIONS" });
+            return axios.get('http://localhost:3000/reunions')
+            .then(data => {
+                dispatch({ type: "ADD_REUNIONS", reunions: data });
+            });
+        }
+    }
