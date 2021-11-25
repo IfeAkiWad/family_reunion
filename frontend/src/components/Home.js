0c05b4c4ae31258d1cd6a77eccc0247cbb67eed4
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-11-25 16:46:37 -0600

    built Home component and CSS for Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Family from '../assets/Family.gif'
 
 const Home = () => {
     return (
         <div>
-            <h1>It's A Family Reunion! 🎊</h1>
-            <h2>Come make a record of your whole family tree here in your own virtual family reunion</h2>
-            
+            <h1 id='reunion-banner' >It's A Family Reunion!</h1>
+            <h2 id='reunion-banner' >Come make a record of your whole family tree here in your own virtual family reunion</h2>
+            <img id='family-tree' src={Family} alt="Gif..." />
         </div>
     )
 }
