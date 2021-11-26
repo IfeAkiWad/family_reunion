fef5897e461c772bf71410805c3627e9d42389bd
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-11-25 23:02:01 -0600

    CSS /updates for About  and Home component

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,7 +6,8 @@ const Home = () => {
         <div>
             <img id='family-tree' src={Family} alt="Gif..." />
             <h1 id='reunion-h2' >It's A Family Reunion!</h1>
-            <h3 id='reunion-h3' >Come make a record of your whole family tree in your own virtual family reunion</h3>
+
+            {/* <h3 id='reunion-h3' >Come make a record of your whole family tree in your own virtual family reunion</h3> */}
         </div>
     )
 }
